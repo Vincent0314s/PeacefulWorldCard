307d2f3ecd7d2e11fe910e37cff708251f4d6828
Vincent0314s <56059146+Vincent0314s@users.noreply.github.com>
2025-06-17 20:41:32 +1000

    Delete LeanTween package and related logic

--- Assets/Scripts/Controller/PaperScissorRockSequenceController.cs
+++ Assets/Scripts/Controller/PaperScissorRockSequenceController.cs
@@ -4,70 +4,70 @@ using UnityEngine;
 
 public class PaperScissorRockSequenceController : MonoBehaviour
 {
-    [SerializeField] private LeanTweenMovement _paperTweenMovement;
-    [SerializeField] private LeanTweenFading[] _paperTweenFadings;
+    //[SerializeField] private LeanTweenMovement _paperTweenMovement;
+    //[SerializeField] private LeanTweenFading[] _paperTweenFadings;
 
-    [SerializeField] private LeanTweenFading[] _scissorsTweenFadings;
+    //[SerializeField] private LeanTweenFading[] _scissorsTweenFadings;
 
-    [SerializeField] private LeanTweenMovement _rockTweenMovement;
-    [SerializeField] private LeanTweenFading[] _rockTweenFadings;
+    //[SerializeField] private LeanTweenMovement _rockTweenMovement;
+    //[SerializeField] private LeanTweenFading[] _rockTweenFadings;
 
     public void PaperSelection()
     {
-        for (int i = 0; i < _scissorsTweenFadings.Length; i++)
-        {
-            _scissorsTweenFadings[i].FadeOut();
-        }
-        for (int i = 0; i < _rockTweenFadings.Length; i++)
-        {
-            _rockTweenFadings[i].FadeOut();
-        }
-        _paperTweenMovement.MoveToDestination();
+        //for (int i = 0; i < _scissorsTweenFadings.Length; i++)
+        //{
+        //    _scissorsTweenFadings[i].FadeOut();
+        //}
+        //for (int i = 0; i < _rockTweenFadings.Length; i++)
+        //{
+        //    _rockTweenFadings[i].FadeOut();
+        //}
+        //_paperTweenMovement.MoveToDestination();
     }
 
     public void ScissorsSelection()
     {
-        for (int i = 0; i < _paperTweenFadings.Length; i++)
-        {
-            _paperTweenFadings[i].FadeOut();
-        }
-        for (int i = 0; i < _rockTweenFadings.Length; i++)
-        {
-            _rockTweenFadings[i].FadeOut();
-        }
+        //for (int i = 0; i < _paperTweenFadings.Length; i++)
+        //{
+        //    _paperTweenFadings[i].FadeOut();
+        //}
+        //for (int i = 0; i < _rockTweenFadings.Length; i++)
+        //{
+        //    _rockTweenFadings[i].FadeOut();
+        //}
     }
 
     public void RockSelection()
     {
-        for (int i = 0; i < _paperTweenFadings.Length; i++)
-        {
-            _paperTweenFadings[i].FadeOut();
-        }
-        for (int i = 0; i < _scissorsTweenFadings.Length; i++)
-        {
-            _scissorsTweenFadings[i].FadeOut();
-        }
-        _rockTweenMovement.MoveToDestination();
+        //for (int i = 0; i < _paperTweenFadings.Length; i++)
+        //{
+        //    _paperTweenFadings[i].FadeOut();
+        //}
+        //for (int i = 0; i < _scissorsTweenFadings.Length; i++)
+        //{
+        //    _scissorsTweenFadings[i].FadeOut();
+        //}
+        //_rockTweenMovement.MoveToDestination();
     }
 
     public void ResetSequence()
     {
-        _paperTweenMovement.ResetPosition();
-        _rockTweenMovement.ResetPosition();
+        //_paperTweenMovement.ResetPosition();
+        //_rockTweenMovement.ResetPosition();
 
-        for (int i = 0; i < _paperTweenFadings.Length; i++)
-        {
-            _paperTweenFadings[i].ResetAlpha();
-        }
+        //for (int i = 0; i < _paperTweenFadings.Length; i++)
+        //{
+        //    _paperTweenFadings[i].ResetAlpha();
+        //}
 
-        for (int i = 0; i < _scissorsTweenFadings.Length; i++)
-        {
-            _scissorsTweenFadings[i].ResetAlpha();
-        }
+        //for (int i = 0; i < _scissorsTweenFadings.Length; i++)
+        //{
+        //    _scissorsTweenFadings[i].ResetAlpha();
+        //}
 
-        for (int i = 0; i < _rockTweenFadings.Length; i++)
-        {
-            _rockTweenFadings[i].ResetAlpha();
-        }
+        //for (int i = 0; i < _rockTweenFadings.Length; i++)
+        //{
+        //    _rockTweenFadings[i].ResetAlpha();
+        //}
     }
 }
